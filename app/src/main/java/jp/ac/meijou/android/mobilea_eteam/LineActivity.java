c97c193c7698b6955ab99d326765c3e7879168b4
sakashitatsubasa <115210858+sakashitatsubasa@users.noreply.github.com>
2023-12-06 09:40:41 +0900

    共通のレイアウト，ボタン操作をmain以外に反映

--- app/src/main/java/jp/ac/meijou/android/mobilea_eteam/LineActivity.java
+++ app/src/main/java/jp/ac/meijou/android/mobilea_eteam/LineActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import jp.ac.meijou.android.mobilea_eteam.databinding.ActivityLineBinding;
+import jp.ac.meijou.android.mobilea_eteam.databinding.BottomButtonBinding;
+
 import android.graphics.Color;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +24,7 @@ import java.util.List;
 public class LineActivity extends AppCompatActivity {
 
     private ActivityLineBinding binding;
+    private ButtonClickListener buttonClickListener;
     private LineChart lineChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +32,9 @@ public class LineActivity extends AppCompatActivity {
         binding = ActivityLineBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.button5.setOnClickListener(view -> {
-            var intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        });
+        //BottomButtonBinding bottomButtonBinding = BottomButtonBinding.inflate(getLayoutInflater());
+
+
 
         List<Float> x = new ArrayList<>(Arrays.asList(1f, 2f, 3f, 5f, 8f, 13f, 21f, 34f)); // X軸データ
         List<Float> y = new ArrayList<>();
@@ -74,5 +76,12 @@ public class LineActivity extends AppCompatActivity {
 
         // LineChart更新
         lineChart.invalidate();
+
+        buttonClickListener = new ButtonClickListener(this);
+
+        binding.includedLayout.button.setOnClickListener(view -> buttonClickListener.onButtonClick(MainActivity.class));
+        binding.includedLayout.button4.setOnClickListener(view -> buttonClickListener.onButtonClick(totalActivity.class));
+        binding.includedLayout.button2.setOnClickListener(view -> buttonClickListener.onButtonClick(PieActivity.class));
+
     }
 }
